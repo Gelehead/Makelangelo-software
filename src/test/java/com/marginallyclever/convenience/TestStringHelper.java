11f4c878ed3c66bbf28cf5d785291cc5a22d75e7
Thorantt <86085299+Thorantt@users.noreply.github.com>
2024-10-10 14:00:28 -0400

    tests et leurs explication

--- src/test/java/com/marginallyclever/convenience/TestStringHelper.java
+++ src/test/java/com/marginallyclever/convenience/TestStringHelper.java
@@ -7,11 +7,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestStringHelper {
     @Test
+    public void testGetElapsedTime() {
+        int testTime = 16000;
+        String result = "04:26:04";
+        assertEquals(StringHelper.getElapsedTime(testTime), result);
+    }
+
+    @Test
     public void testPaddedHex() {
         assertEquals("000001", StringHelper.paddedHex(0x1));
-        assertEquals("0007ff",StringHelper.paddedHex(0x7ff));
-        assertEquals("100001",StringHelper.paddedHex(0x100001));
-        assertEquals("1000001",StringHelper.paddedHex(0x1000001));
-        assertEquals("100000",StringHelper.paddedHex(0x100000));
+        assertEquals("0007ff", StringHelper.paddedHex(0x7ff));
+        assertEquals("100001", StringHelper.paddedHex(0x100001));
+        assertEquals("1000001", StringHelper.paddedHex(0x1000001));
+        assertEquals("100000", StringHelper.paddedHex(0x100000));
     }
 }
--- src/test/java/com/marginallyclever/convenience/helpers/MathHelperTest.java
+++ src/test/java/com/marginallyclever/convenience/helpers/MathHelperTest.java
@@ -6,6 +6,32 @@ import org.junit.jupiter.api.Test;
 import javax.vecmath.Point2d;
 
 public class MathHelperTest {
+
+    @Test
+    public void testEquals() {
+        Point2d a0 = new Point2d();
+        Point2d a1 = new Point2d();
+        Point2d b0 = new Point2d();
+        Point2d b1 = new Point2d();
+        double epsilon = 1e-9;
+
+        double p1x = Math.random() * 100;
+        double p1y = Math.random() * 50;
+        double p2x = Math.random() * 110;
+        double p2y = Math.random() * 55;
+
+        a0.set(p1x, p1y);
+        a1.set(p2x, p2y);
+        b1.set(p2x, p2y);
+        b0.set(p1x, p1y);
+
+        boolean l = MathHelper.equals(a0, a1, b0, b1, epsilon);
+        b1.set(p1x, p1y);
+        b0.set(p2x, p2y);
+        boolean m = MathHelper.equals(a0, a1, b0, b1, epsilon);
+        assert (l && m);
+    }
+
     @Test
     public void testBetween() {
         Point2d a = new Point2d();
@@ -28,17 +54,17 @@ public class MathHelperTest {
         Point2d c;
         double epsilon = 1e-9;
 
-        for(int i=0;i<50;++i) {
-            a.set(Math.random()*500-250, Math.random()*500-250);
-            b.set(Math.random()*500-250, Math.random()*500-250);
-            c = MathHelper.lerp(a,b,1.0+epsilon+Math.random());
+        for (int i = 0; i < 50; ++i) {
+            a.set(Math.random() * 500 - 250, Math.random() * 500 - 250);
+            b.set(Math.random() * 500 - 250, Math.random() * 500 - 250);
+            c = MathHelper.lerp(a, b, 1.0 + epsilon + Math.random());
             Assertions.assertFalse(MathHelper.between(a, b, c, epsilon));
         }
 
-        for(int i=0;i<50;++i) {
-            a.set(Math.random()*500-250, Math.random()*500-250);
-            b.set(Math.random()*500-250, Math.random()*500-250);
-            c = MathHelper.lerp(a,b,-epsilon-Math.random());
+        for (int i = 0; i < 50; ++i) {
+            a.set(Math.random() * 500 - 250, Math.random() * 500 - 250);
+            b.set(Math.random() * 500 - 250, Math.random() * 500 - 250);
+            c = MathHelper.lerp(a, b, -epsilon - Math.random());
             Assertions.assertFalse(MathHelper.between(a, b, c, epsilon));
         }
     }
@@ -51,36 +77,36 @@ public class MathHelperTest {
         Point2d c;
         double epsilon = 1e-9;
 
-        for(int i=0;i<50;++i) {
-            a.set(Math.random()*500-250, Math.random()*500-250);
-            b.set(Math.random()*500-250, Math.random()*500-250);
-            c = MathHelper.lerp(a,b,Math.random());
+        for (int i = 0; i < 50; ++i) {
+            a.set(Math.random() * 500 - 250, Math.random() * 500 - 250);
+            b.set(Math.random() * 500 - 250, Math.random() * 500 - 250);
+            c = MathHelper.lerp(a, b, Math.random());
             ortho.set(b);
             ortho.sub(a);
-            c.x+=ortho.y;
-            c.y+=ortho.x;
+            c.x += ortho.y;
+            c.y += ortho.x;
             Assertions.assertFalse(MathHelper.between(a, b, c, epsilon));
         }
 
-        for(int i=0;i<50;++i) {
-            a.set(Math.random()*500-250, Math.random()*500-250);
-            b.set(Math.random()*500-250, Math.random()*500-250);
-            c = MathHelper.lerp(a,b,1.0+epsilon+Math.random());
+        for (int i = 0; i < 50; ++i) {
+            a.set(Math.random() * 500 - 250, Math.random() * 500 - 250);
+            b.set(Math.random() * 500 - 250, Math.random() * 500 - 250);
+            c = MathHelper.lerp(a, b, 1.0 + epsilon + Math.random());
             ortho.set(b);
             ortho.sub(a);
-            c.x+=ortho.y;
-            c.y+=ortho.x;
+            c.x += ortho.y;
+            c.y += ortho.x;
             Assertions.assertFalse(MathHelper.between(a, b, c, epsilon));
         }
 
-        for(int i=0;i<50;++i) {
-            a.set(Math.random()*500-250, Math.random()*500-250);
-            b.set(Math.random()*500-250, Math.random()*500-250);
-            c = MathHelper.lerp(a,b,-epsilon-Math.random());
+        for (int i = 0; i < 50; ++i) {
+            a.set(Math.random() * 500 - 250, Math.random() * 500 - 250);
+            b.set(Math.random() * 500 - 250, Math.random() * 500 - 250);
+            c = MathHelper.lerp(a, b, -epsilon - Math.random());
             ortho.set(b);
             ortho.sub(a);
-            c.x+=ortho.y;
-            c.y+=ortho.x;
+            c.x += ortho.y;
+            c.y += ortho.x;
             Assertions.assertFalse(MathHelper.between(a, b, c, epsilon));
         }
     }
