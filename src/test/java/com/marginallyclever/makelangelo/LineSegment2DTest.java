e953b9e71490982cfdf4ffc11811ea6e609e9881
Gelehead <113441042+Gelehead@users.noreply.github.com>
2024-11-12 17:01:34 -0500

    believe

---
+++ src/test/java/com/marginallyclever/makelangelo/LineSegment2DTest.java
@@ -0,0 +1,13 @@
+package com.marginallyclever.makelangelo;
+
+class LineSegment2DTest {
+    public boolean assertflipped(){
+        LineSegment2D line = new  
+    }
+
+
+    @Test
+    public void test(){
+
+    }
+} 
